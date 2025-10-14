a20691b0d1ea1e2190630f56cc3aaae26d6b3f17
AleMiclos <159150312+AleMiclos@users.noreply.github.com>
2025-10-14 20:43:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- const API_URL = 'https://winners-api-delta.vercel.app/api';
+ const API_URL = 'https://winnersapi-g031.onrender.com';
         const startScreen = document.getElementById('startScreen');
         const gameScreen = document.getElementById('gameScreen');
         const gameOverScreen = document.getElementById('gameOverScreen');
