2c117b9775cee59d62184e37c2d34a0914c2875e
AleMiclos <alexandremiclos@gmail.com>
2025-10-15 11:42:07 -0300

    nova rota

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- const API_URL = 'https://winners-api-delta.vercel.app/api';
+ const API_URL = 'https://winnersapi-1.onrender.com/api';
         const startScreen = document.getElementById('startScreen');
         const gameScreen = document.getElementById('gameScreen');
         const gameOverScreen = document.getElementById('gameOverScreen');
